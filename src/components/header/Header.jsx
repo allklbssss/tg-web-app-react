a465fd83b197521aee4bfd47efeb572b585c8b6c
Алина Колбасова <alinakolbasova@MacBook-Air-Alina.local>
2024-04-23 12:55:15 +0300

    add header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../button/Button";
-import { useTelegram } from "../../ hooks/useTelegram";
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
    
