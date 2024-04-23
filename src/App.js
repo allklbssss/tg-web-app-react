325ee3277a821ab9facbeab784e1e5426c735d45
Алина Колбасова <alinakolbasova@MacBook-Air-Alina.local>
2024-04-23 15:10:00 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import { useEffect } from 'react';
 import './App.css';
-import { useTelegram } from "./hooks/useTelegram";
-import Header from './components/header/Header';
-import { Routes, Route } from 'react-router-dom';
-import ProductList from './components/productList/ProductList';
-import Form from './components/form/Form';
-
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/productList/ProductList";
+import Form from "./components/form/Form";
 
 function App() {
+    const {tg} = useTelegram();
 
-  const {onToggleButton, tg} = useTelegram()
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  useEffect(()=> {
-    tg.ready()
-  }, [])
-
-  return (
-    <div className="App">
-      <Header/>
-    <Routes>
-      <Route index element={<ProductList />} />
-      <Route path={'form'} element={<Form />} />
-    </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import Button from "../button/Button";
-import { useTelegram } from "../../hooks/useTelegram";
-import './Header.css'
+import React from 'react';
+import Button from "../Button/Button";
+import {useTelegram} from "../../hooks/useTelegram";
+import './Header.css';
 
 const Header = () => {
-   
-    const {user, onClose} = useTelegram()
+    const {user, onClose} = useTelegram();
 
     return (
         <div className={'header'}>
-             <Button onClick={onClose}>Закрыть </Button>
-             <span className={'username'}>{user?.username}</span>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {user?.username}
+            </span>
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
